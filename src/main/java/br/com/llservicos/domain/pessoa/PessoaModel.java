3ce11f306f17b171d1e200b275df1a94be9d11d7
VictorDays <victoralvesrdias@gmail.com>
2024-09-22 14:37:32 -0300

    Alterando a estrategia da classe pai

--- src/main/java/br/com/llservicos/domain/pessoa/PessoaModel.java
+++ src/main/java/br/com/llservicos/domain/pessoa/PessoaModel.java
@@ -1,11 +1,9 @@
 package br.com.llservicos.domain.pessoa;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 
 @Entity
-@MappedSuperclass
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class PessoaModel {
 
     @Id
