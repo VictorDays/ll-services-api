c543e3c57dd83593b10f1f0049bd498373f5624f
Emily Bessa <emilymaria@unitins.br>
2024-09-29 20:39:20 -0300

    Create EnderecoDTO.java
    criando endereço DTO

---
+++ src/main/java/br/com/llservicos/domain/endereco/EnderecoDTO.java
@@ -0,0 +1,14 @@
+package br.com.llservicos.domain.endereco;
+import jakarta.validation.constraints.NotBlank;
+
+
+public record EnderecoDTO(
+        @NotBlank(message = "O campo nome não pode ser nulo.") String nome,
+        @NotBlank(message = "O campo logradouro não pode ser nulo.") String logradouro,
+        String complemento,
+        @NotBlank(message = "O campo bairro não pode ser nulo.") String bairro,
+        Integer numero,
+        @NotBlank(message = "O campo CEP não pode ser nulo.") String cep,
+        @NotBlank(message = "O campo cidade não pode ser nulo.") String cidade,
+        @NotBlank(message = "O campo estado não pode ser nulo.") String estado) {
+}
