120c58f42f7cea702d78d25d6ae277f969d775e1
Emily Bessa <emilymaria@unitins.br>
2024-10-13 22:30:36 -0300

    Create EnderecoResponseDTO.java

---
+++ src/main/java/br/com/llservicos/domain/endereco/dto/EnderecoResponseDTO.java
@@ -0,0 +1,30 @@
+package br.com.llservicos.domain.endereco.dto;
+
+import br.com.llservicos.domain.endereco.EnderecoModel;
+
+public record EnderecoResponseDTO(
+    Long id,
+    String nome,
+    String logradouro,
+    String complemento,
+    String bairro,
+    Integer numero,
+    String cep,
+    String cidade,
+    String estado
+
+) {
+    public static EnderecoResponseDTO valueOf(EnderecoModel endereco){
+        return new EnderecoResponseDTO(
+            endereco.getId(),
+            endereco.getNome(),
+            endereco.getLogadouro(),
+            endereco.getComplemento(),
+            endereco.getBairro(),
+            endereco.getNumero(),
+            endereco.getCep(),
+            endereco.getCidade(),
+            endereco.getEstado()
+        );
+    }
+}
