f85a830986cf3177bd6e9b012eba7b652d780e21
Victor Dias <70988788+VictorDays@users.noreply.github.com>
2024-10-17 09:17:23 -0300

    Configuração do banco de dados e alteração da porta

--- src/main/java/br/com/llservicos/repositories/EnderecoRepository.java
+++ src/main/java/br/com/llservicos/repositories/EnderecoRepository.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import br.com.llservicos.domain.endereco.EnderecoModel;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
 
+@ApplicationScoped
 public class EnderecoRepository implements PanacheRepository<EnderecoModel> {
 
     public List<EnderecoModel> findByCep(String cep) {
