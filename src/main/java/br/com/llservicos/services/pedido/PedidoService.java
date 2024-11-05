b141c177a63744cd525c95fc105ff36fd0fca3d9
Victor Dias <70988788+VictorDays@users.noreply.github.com>
2024-11-05 09:10:06 -0300

    Add componentes do pedido

---
+++ src/main/java/br/com/llservicos/services/pedido/PedidoService.java
@@ -0,0 +1,42 @@
+package br.com.llservicos.services.pedido;
+
+import java.util.List;
+import java.util.Optional;
+
+import br.com.llservicos.domain.pedido.PedidoModel;
+import br.com.llservicos.domain.pedido.dtos.PedidoDTO;
+
+
+public interface PedidoService {
+
+public PedidoModel createPedido(PedidoDTO pedidoDTO);
+
+public PedidoModel updatePedido(Long id, PedidoDTO pedidoDTO);
+
+public boolean deletePedido(Long id);
+
+public PedidoModel getPedidoById(Long id);
+
+public List<PedidoModel> getAllPedidos();
+
+public List<PedidoModel> getPedidosByPessoaId(Long pessoaId);
+
+public List<PedidoModel> getPedidosByStatus(String status);
+
+public List<PedidoModel> getPedidosByDataServico(String dataServico);
+
+public List<PedidoModel> getPedidosByValorTotal(Double valorTotal);
+
+PedidoModel save(PedidoModel pedido);
+
+List<PedidoModel> findAll();
+
+void deleteById(Long id);
+
+Optional<PedidoModel> findById(Long id);
+
+}
+   
+
+
+
---
+++ src/main/java/br/com/llservicos/services/pedido/PedidoServiceImpl.java
@@ -0,0 +1,115 @@
+package br.com.llservicos.services.pedido;
+
+import java.util.List;
+import java.util.Optional;
+
+import br.com.llservicos.domain.pedido.PedidoModel;
+import br.com.llservicos.domain.pedido.dtos.PedidoDTO;
+
+import br.com.llservicos.repositories.PedidoRepository;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+
+
+@ApplicationScoped
+public class PedidoServiceImpl implements PedidoService{
+   
+    @Inject
+    PedidoRepository pedidoRepository;
+
+    
+
+    @Override
+    @Transactional
+    public PedidoModel createPedido(PedidoDTO pedidoDTO) {
+        PedidoModel pedido = new PedidoModel(0, null, null, null, null, null);
+        pedido.setPessoa(pedidoDTO.pessoa());
+        pedido.setStatus(pedidoDTO.status());
+        pedido.setValorTotal(pedidoDTO.valorTotal());
+
+        pedidoRepository.persist(pedido);
+        return pedido;
+    }
+
+    @Override
+    @Transactional
+    public PedidoModel updatePedido(Long id, PedidoDTO pedidoDTO) {
+        PedidoModel pedido = pedidoRepository.findById(id);
+        if (pedido == null) {
+            throw new RuntimeException("Pedido não encontrado");
+        }
+
+        
+        pedido.setPessoa(pedidoDTO.pessoa());
+        pedido.setStatus(pedidoDTO.status());
+        pedido.setValorTotal(pedidoDTO.valorTotal());
+
+        return pedido;
+    }
+
+    @Override
+    @Transactional
+    public boolean deletePedido(Long id) {
+        return pedidoRepository.deleteById(id);
+    }
+
+    @Override
+    public PedidoModel getPedidoById(Long id) {
+        PedidoModel pedido = pedidoRepository.findById(id);
+        if (pedido == null) {
+            throw new RuntimeException("Pedido não encontrado");
+        }
+        return pedido;
+    }
+
+    @Override
+    public List<PedidoModel> getAllPedidos() {
+        return pedidoRepository.findAll().list();
+    }
+
+    @Override
+    public List<PedidoModel> getPedidosByPessoaId(Long pessoaId) {
+        return pedidoRepository.find("pessoaId", pessoaId).list();
+    }
+
+    @Override
+    public List<PedidoModel> getPedidosByStatus(String status) {
+        return pedidoRepository.find("status", status).list();
+    }
+
+    @Override
+    public List<PedidoModel> getPedidosByDataServico(String dataServico) {
+        return pedidoRepository.find("dataServico", dataServico).list();
+    }
+
+    @Override
+    public List<PedidoModel> getPedidosByValorTotal(Double valorTotal) {
+        return pedidoRepository.find("valorTotal", valorTotal).list();
+    }
+    
+
+     @Override
+    @Transactional
+    public PedidoModel save(PedidoModel pedido) {
+        pedidoRepository.persist(pedido);
+        return pedido;
+    }
+
+    @Override
+    public Optional<PedidoModel> findById(Long id) {
+        return pedidoRepository.findByIdOptional(id);
+    }
+
+    @Override
+    public List<PedidoModel> findAll() {
+        return pedidoRepository.listAll();
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(Long id) {
+    pedidoRepository.deleteById(id); 
+    }
+}
