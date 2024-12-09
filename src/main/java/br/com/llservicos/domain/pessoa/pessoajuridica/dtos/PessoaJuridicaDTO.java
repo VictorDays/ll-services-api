6796d32339c70529741da23fae9988d4e591b396
Victor Dias <70988788+VictorDays@users.noreply.github.com>
2024-12-09 10:05:56 -0300

    Bot finalizado

--- src/main/java/br/com/llservicos/domain/pessoa/pessoajuridica/dtos/PessoaJuridicaDTO.java
+++ src/main/java/br/com/llservicos/domain/pessoa/pessoajuridica/dtos/PessoaJuridicaDTO.java
@@ -6,11 +6,82 @@ import jakarta.validation.constraints.NotBlank;
 
 import java.util.List;
 
-public record PessoaJuridicaDTO(
-        @NotBlank(message = "O campo nome não pode ser nulo.") String nome,
-        String cnpj,
-        @Email(message = "O e-mail deve ser válido.") String email,
-        String telefone,
-        List<EnderecoDTO> enderecos
-) {
+public class PessoaFisicaDTO {
+    @NotBlank(message = "O campo nome não pode ser nulo.")
+    private String nome;
+
+    private String cpf;
+
+    @Email(message = "O e-mail deve ser válido.")
+    private String email;
+
+    private String telefone;
+
+    private List<EnderecoDTO> enderecos;
+
+    // Construtor padrão
+    public PessoaFisicaDTO() {
+    }
+
+    // Construtor com todos os argumentos
+    public PessoaFisicaDTO(String nome, String cpf, String email, String telefone, List<EnderecoDTO> enderecos) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+        this.enderecos = enderecos;
+    }
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public List<EnderecoDTO> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(List<EnderecoDTO> enderecos) {
+        this.enderecos = enderecos;
+    }
+
+    // Método toString (opcional, útil para depuração)
+    @Override
+    public String toString() {
+        return "PessoaFisicaDTO{" +
+                "nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", email='" + email + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", enderecos=" + enderecos +
+                '}';
+    }
 }
